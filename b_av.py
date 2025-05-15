dabb0288dd63de095f6c703b89cd5072ee111a0c
ckechios <62009820+ckechios@users.noreply.github.com>
2025-05-15 18:29:34 +0000

    End of session

--- b_av.py
+++ b_av.py
@@ -2,6 +2,7 @@ import requests
 from dotenv import load_dotenv
 import os
 
+# create a .env file add AV_KEY=yourkey
 load_dotenv()
 
 API_URL = "https://www.alphavantage.co/query"
@@ -23,5 +24,6 @@ rr = response.json()
 data = rr['Time Series (Daily)']
 
 for k in data:
-    print(k, " = ", data[k])
+    # print(k, " = ", data[k]["4. close"], type(data[k]["4. close"]))
+    print(f"{k} = ${float(data[k]["4. close"]):.2f}")
     # 2024-12-18 = 220.1700
